c227d97fef381890083066a640fbbece7beafc66
Scherbi <shherbakov99ilya@mail.ru>
2023-07-31 18:56:48 +0300

    Groovy

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,4 +1,15 @@
-static void main(String[] args) {
-  println "Hello world!"
-  
-}
+
+   def countElements(list) {
+    def countMap = [:]
+
+    list.each { element ->
+      countMap[element] = countMap.get(element, 0) + 1
+    }
+
+    return countMap
+  }
+
+  def list = [1, 2, 4, 2, 1, 5, 4]
+  def result = countElements(list)
+
+  println result
